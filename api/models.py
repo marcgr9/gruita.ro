93b60269066e81856c76974369210f8213cbb0b8
Marc <marcgruita@gmail.com>
2021-10-04 23:29:05 +0300

    altered some db fields for better ui

--- api/models.py
+++ api/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Project(models.Model):
-    title = models.CharField(max_length=30, blank=False)
-    short_description = models.CharField(max_length=30, blank=False)
+    title = models.CharField(max_length=30)
+    short_description = models.CharField(max_length=30)
     long_description = models.CharField(max_length=150)
-    github = models.URLField(default="")
-    link = models.CharField(max_length=50, default="", blank=True)
+    github = models.URLField(max_length=70, default="", blank=True)
+    link = models.CharField(max_length=70, default="", blank=True)
     icon = models.CharField(max_length=20)
     color = models.CharField(max_length=20)
     languages = models.CharField(max_length=10)
@@ -16,7 +16,7 @@ class Project(models.Model):
 class Technology(models.Model):
     category = models.CharField(max_length=30, blank=False, unique=False)
     name = models.CharField(max_length=50, unique=True)
-    description = models.CharField(max_length=120)
+    description = models.CharField(max_length=120, default="", blank=True)
     project = models.CharField(max_length=50, blank=True, default="")
     link = models.CharField(max_length=150, blank=True)
     languages = models.CharField(max_length=20, blank=True, default="")
